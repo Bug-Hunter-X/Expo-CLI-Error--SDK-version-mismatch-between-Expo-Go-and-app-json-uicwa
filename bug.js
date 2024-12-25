8715625068981f193f50353120489d4ae5203448
BugHunter-X <askzenai@gmail.com>
2024-12-25 14:34:07 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,1 @@
+This error typically arises when there's a mismatch between the Expo Go app version on your device and the SDK version specified in your app's `app.json` file.  Expo Go has a specific range of SDK versions it supports; if your app's SDK version is outside this range, the error occurs.  Another cause could be an issue with your project's dependencies or a corrupted project setup.
